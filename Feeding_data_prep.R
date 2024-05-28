0d70a071a984beb7163c3405c2555a23bc27d9c7
Steffen Oppel <steffen.oppel@gmail.com>
2024-05-28 16:03:21 +0200

    created loop over recurse to avoid memory error
    lapply function did not run due to memory allocation error

--- Feeding_data_prep.R
+++ Feeding_data_prep.R
@@ -91,6 +91,9 @@ track_amt <- track_sf %>%
   time_of_day(include.crepuscule = T) %>% # if F, crepuscule is considered as night
   arrange(id, t_)
 
+## clean up workspace
+rm(trackingdata,track_sf)
+gc()
 
 ### CALCULATE OTHER METRICS
 track_amt$step_length<-amt::step_lengths(track_amt)       # include step lengths
@@ -101,16 +104,53 @@ head(track_amt)
 
 
 # RECURSIONS FOR EACH LOCATION 50 m BUFFER--------------------------------------------------
-# splitting track into a list with each single id grouped to an element
+# splitting track into a list with each single id grouped to an element------
+# this causes a memory limit error, so try and do it in a loop
+
 track_amt <- as.data.frame(track_amt)
-track_amt_list <- split(track_amt, track_amt$id)
+#track_amt_list <- split(track_amt, track_amt$id)
+
+
+
 
 # calculating recursions
 # rm(trackingdata,track_sf,buildings,forest)  ### clean up workspace and memory
 # gc()
 
-track_amt_recurse <- lapply(track_amt_list, function(x)
-  getRecursions(x = x[1:4], radius = 50, timeunits = "hours"))
+# track_amt_recurse <- lapply(track_amt_list, function(x)
+#   getRecursions(x = x[1:4], radius = 50, timeunits = "hours"))
+track_amt$revisits <- NA
+track_amt$residence_time <- NA
+for (i in unique(track_amt$id)){
+  x<-track_amt %>% filter(id==i)
+  xr<-getRecursions(x = x[1:4], radius = 50, timeunits = "hours")
+  
+  track_amt$revisits[track_amt$id == i] <- xr$revisits
+  track_amt$residence_time[track_amt$id == i] <- xr$residenceTime
+  
+  # CALCULATING FIRST AND LAST REVISIT AND DURATION AND TEMPORAL PERSISTENCE OF REVISITS -----------------------------------------------------------------
+  tempout<-
+    xr$revisitStats %>%
+    mutate(jday.ent=yday(entranceTime),jday.ex=yday(exitTime)) %>%
+    group_by(coordIdx) %>%
+    summarise(first=min(entranceTime, na.rm=T),
+              last=max(exitTime, na.rm=T),
+              meanFreqVisit=mean(timeSinceLastVisit, na.rm=T),
+              n_days=length(unique(c(jday.ent,jday.ex)))) %>%
+    mutate(TimeSpan=as.numeric(difftime(last,first,unit="days"))) %>%
+    mutate(TempEven=n_days/TimeSpan) %>%
+    mutate(meanFreqVisit=ifelse(is.na(meanFreqVisit),0,meanFreqVisit)) %>%   ## set the frequency of visit to 0 for locations never revisited
+    mutate(TempEven=ifelse(n_days==1,1,TempEven)) %>%   ## set the evenness to 1 for locations never revisited on more than a single day
+    select(meanFreqVisit,n_days,TimeSpan,TempEven)
+  track_amt$meanFreqVisit[track_amt$id == i] <-tempout$meanFreqVisit
+  track_amt$n_days[track_amt$id == i] <-tempout$n_days
+  track_amt$TimeSpan[track_amt$id == i] <-tempout$TimeSpan
+  track_amt$TempEven[track_amt$id == i] <-tempout$TempEven
+  
+}
+
+
+
 
 # allocating recurse information to track data frame (15 mins)
 track_amt$revisits <- NA
@@ -155,8 +195,6 @@ head(track_amt)
 
 
 # COMBINING DATA WITH FORESTS AND BUILDINGS -----------------------------------------------------------------
-
-
 
 #trackingdata<-fread("C:/Users/sop/OneDrive - Vogelwarte/REKI/Analysis/NestTool/REKI/output/02_preprocessing/03_milvus_combined.csv")
 indseasondata<-fread("C:/Users/sop/OneDrive - Vogelwarte/REKI/Analysis/NestTool/REKI/output/01_validation/03_validation_combined.csv")
