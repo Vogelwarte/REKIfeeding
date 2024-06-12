6438085956cef7c901917e63ac3ad44e9db3d66c
Steffen Oppel <steffen.oppel@gmail.com>
2024-06-12 14:11:08 +0200

    need to re-prepare data because projection shows it is for study area only
    must have filtered out data before _ need to go back to data prep

--- Feeding_projection.R
+++ Feeding_projection.R
@@ -6,6 +6,8 @@
 # created by steffen.oppel@vogelwarte.ch in July 2023
 # building and forest layers provided by Jerome Guelat
 
+# fully revised in June 2024 to include high resolution data
+
 library(tidyverse)
 library(dplyr, warn.conflicts = FALSE)
 options(dplyr.summarise.inform = FALSE)
@@ -55,191 +57,24 @@ forest_CH <- st_read("data/wald.gpkg", "wald") %>%
 
 
 
-###### REGENERATE TRACKING DATA FOR ALL OF SWITZERLAND -----------------------------------------------------------------
-# 
-# ### LOAD THE TRACKING DATA AND INDIVIDUAL SEASON SUMMARIES
-# trackingdata<-readRDS(file = "data/REKI_trackingdata_raw.rds")
-# 
-# # keeping only the information of relevant locations in Switzerland
-# 
-# if("long_wgs" %in% names(trackingdata)){
-#   trackingdata<-trackingdata %>% rename(long=long_wgs, lat=lat_wgs)
-# }
-# trackingdata <- trackingdata %>%
-#   filter(long>5.9) %>%
-#   filter(lat>45.8) %>%
-#   filter(long<10.6) %>%
-#   filter(lat<48) %>%
-#   filter(!is.na(timestamp)) %>%
-#   filter(!is.na(long)) %>%
-#   filter(!is.na(year_id))
-# 
-# dim(trackingdata)
-# 
-# 
-# # converting to metric CRS prior to estimating distances
-# track_sf <- trackingdata %>% 
-#   st_as_sf(coords = c("long", "lat"))
-# st_crs(track_sf) <- 4326
-# 
-# track_sf <- track_sf %>%
-#   st_transform(crs = 3035) %>%
-#   dplyr::mutate(long_eea = sf::st_coordinates(.)[,1],
-#                 lat_eea = sf::st_coordinates(.)[,2])
-# 
-# head(track_sf)
-# 
-# 
-# # Creating a track to exclude nocturnal locations (later on)
-# # 3 mins
-# track_amt <- track_sf %>%
-#   mk_track(
-#     .x = long_eea,
-#     .y = lat_eea,
-#     .t = timestamp,
-#     id = year_id,
-#     crs = 3035
-#   ) %>%
-#   time_of_day(include.crepuscule = T) %>% # if F, crepuscule is considered as night
-#   arrange(id, t_)
-# 
-# ### CALCULATE OTHER METRICS
-# track_amt$step_length<-amt::step_lengths(track_amt)       # include step lengths
-# track_amt$turning_angle<-amt::direction_abs(track_amt,append_last=T)      # include turning angles
-# track_amt$speed<-amt::speed(track_amt)      # include speed
-# 
-# head(track_amt)
-# 
-# 
-# # RECURSIONS FOR EACH LOCATION 50 m BUFFER--------------------------------------------------
-# # splitting track into a list with each single id grouped to an element
-# track_amt <- as.data.frame(track_amt)
-# track_amt_list <- split(track_amt, track_amt$id)
-# 
-# 
-# track_amt_recurse <- lapply(track_amt_list, function(x)
-#   getRecursions(x = x[1:4], radius = 50, timeunits = "hours"))
-# 
-# # allocating recurse information to track data frame (15 mins)
-# track_amt$revisits <- NA
-# track_amt$residence_time <- NA
-# for (i in 1:length(track_amt_recurse)) {
-#   track_amt$revisits[track_amt$id == unique(track_amt$id)[i]] <-
-#     track_amt_recurse[[i]]$revisits
-#   track_amt$residence_time[track_amt$id == unique(track_amt$id)[i]] <-
-#     track_amt_recurse[[i]]$residenceTime
-#   
-#   # CALCULATING FIRST AND LAST REVISIT AND DURATION AND TEMPORAL PERSISTENCE OF REVISITS -----------------------------------------------------------------
-#   tempout<-
-#     track_amt_recurse[[i]]$revisitStats %>%
-#     mutate(jday.ent=yday(entranceTime),jday.ex=yday(exitTime)) %>%
-#     group_by(coordIdx) %>%
-#     summarise(first=min(entranceTime, na.rm=T),
-#               last=max(exitTime, na.rm=T),
-#               meanFreqVisit=mean(timeSinceLastVisit, na.rm=T),
-#               n_days=length(unique(c(jday.ent,jday.ex)))) %>%
-#     mutate(TimeSpan=as.numeric(difftime(last,first,unit="days"))) %>%
-#     mutate(TempEven=n_days/TimeSpan) %>%
-#     mutate(meanFreqVisit=ifelse(is.na(meanFreqVisit),0,meanFreqVisit)) %>%   ## set the frequency of visit to 0 for locations never revisited
-#     mutate(TempEven=ifelse(n_days==1,1,TempEven)) %>%   ## set the evenness to 1 for locations never revisited on more than a single day
-#     select(meanFreqVisit,n_days,TimeSpan,TempEven)
-#   track_amt$meanFreqVisit[track_amt$id == unique(track_amt$id)[i]] <-tempout$meanFreqVisit
-#   track_amt$n_days[track_amt$id == unique(track_amt$id)[i]] <-tempout$n_days
-#   track_amt$TimeSpan[track_amt$id == unique(track_amt$id)[i]] <-tempout$TimeSpan
-#   track_amt$TempEven[track_amt$id == unique(track_amt$id)[i]] <-tempout$TempEven
-# }
-# 
-# 
-# # CALCULATING MOVING AVERAGE FOR SPEED AND ANGLE -----------------------------------------------------------------
-# rm(track_amt_recurse,track_amt_list,trackingdata)
-# track_amt <- track_amt %>% 
-#   arrange(id, t_) %>%
-#   group_by(id) %>%
-#   mutate(mean_speed=frollmean(speed,n=3,na.rm=T, align="center")) %>%
-#   mutate(mean_angle=frollmean(abs(turning_angle),n=5,na.rm=T, align="center")) %>%
-#   mutate(mean_speed=ifelse(is.na(mean_speed),speed,mean_speed)) %>%
-#   mutate(mean_angle=ifelse(is.na(mean_angle),turning_angle,mean_angle)) 
-# head(track_amt)
-# 
-# #trackingdata<-fread("C:/Users/sop/OneDrive - Vogelwarte/REKI/Analysis/NestTool/REKI/output/02_preprocessing/03_milvus_combined.csv")
-# indseasondata<-fread("C:/Users/sop/OneDrive - Vogelwarte/REKI/Analysis/NestTool/REKI/output/01_validation/03_validation_combined.csv")
-# nestdata<-fread("C:/Users/sop/OneDrive - Vogelwarte/REKI/Analysis/NestTool/REKI/data/Basic_nest_list_2015_2022.csv")
-# 
-# ### NESTS
-# nests<- nestdata %>% dplyr::select(nest_name,tree_spec,latitude,longitude) %>%
-#   st_as_sf(coords = c("longitude", "latitude"))
-# st_crs(nests) <- 4326
-# nests<-nests %>% st_transform(crs = 3035)
-# nests_buff<- nests %>%
-#   st_buffer(dist=50)
-# nests_buff
-# 
-# ### remove locations outside of Switzerland
-# dim(track_amt)
-# track_sf <- track_amt %>% 
-#   st_as_sf(coords = c("x_", "y_"))
-# st_crs(track_sf) <- 3035
-# head(track_sf)
-# track_sf <- track_sf %>% st_crop(x=track_sf, y=st_bbox(forest_CH))
-# dim(track_sf)
-# 
-# 
-# ### spatial joins with forests, buildings and feeders
-# head(track_sf)
-# track_sf <- track_sf %>%
-#   st_join(forest_CH,
-#           join = st_intersects, 
-#           left = TRUE) %>%
-#   st_join(buildings_CH,
-#           join = st_intersects, 
-#           left = TRUE) %>%
-#   st_join(nests_buff,
-#           join = st_intersects, 
-#           left = TRUE) %>%
-#   mutate(BUILD=ifelse(is.na(build_id),0,1),
-#          NEST=ifelse(is.na(nest_name),0,1),
-#          FOREST=ifelse(is.na(objektart),0,1)) %>%   
-#   select(-objektart,-nest_name,-tree_spec,-build_id) %>%
-#   rename(building_size=area)
-# 
-# head(track_sf)
-# 
-# st_bbox(forest)
-# st_bbox(buildings)
-# 
-# ### calculate distance to nearest KNOWN nest
-# nest_site_distances<-st_distance(track_sf,nests) 
-# track_sf <- track_sf %>%
-#   mutate(dist_nest=apply(nest_site_distances,1,min)/1000)  ### distance in km
-# 
-# ### join data with individual info
-# track_sf <- track_sf %>% 
-#   st_transform(crs = 4326) %>%
-#   dplyr::mutate(long = sf::st_coordinates(.)[,1],
-#                 lat = sf::st_coordinates(.)[,2]) %>%
-#   # mutate(long = unlist(map(track_sf$geometry,1)),
-#   #        lat = unlist(map(track_sf$geometry,2)))
-#   st_drop_geometry() %>%
-#   rename(year_id=id) %>%
-#   left_join(indseasondata, by="year_id")
-# 
-# saveRDS(track_sf, file = "data/REKI_trackingdata_annotated_CH.rds")
-# head(track_sf)
-# 
-
 ### LOAD THE TRACKING DATA AND INDIVIDUAL SEASON SUMMARIES 
 track_sf<-fread("data/REKI_annotated_feeding2024.csv") %>%
   mutate(extra=year_id) %>%
-  separate_wider_delim(extra, delim="_", names=c("year","bird_id"))
-
+  separate_wider_delim(extra, delim="_", names=c("year","bird_id")) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>%
+  st_transform(3035)
+dim(track_sf)
 
 
 
 ###### APPLY MODELS ACROSS EXTENT OF TRACKING DATA  -----------------------------------------------------------------
 
 
-################ CREATE VARIOUS SUBSETS TO IMPROVE RATIO OF CLASS MEMBERSHIP
-track_nofor_day_build<-track_sf %>% filter(FOREST==0) %>% filter(tod_=="day") %>%
+################ CREATE SUBSETS TO APPLY MODEL TO DAYTIME LOCATIONS OUTSIDE OF FOREST
+track_nofor_day_build<-track_sf %>%
+  st_drop_geometry() %>%
+  filter(FOREST==0) %>%
+  filter(tod_=="day") %>%
   filter(!(BUILD==0)) 
 
 
@@ -255,7 +90,7 @@ DATA_CH <- track_nofor_day_build %>%
   select(-tod_,-FOREST) %>%
   mutate(point_id=seq_along(t_))
 head(DATA_CH)
-
+dim(DATA_CH)
 
 
 # CREATING BASELINE MAP OF SAMPLING INTENSITY -----------------------------------------------------------------
@@ -263,8 +98,7 @@ head(DATA_CH)
 grid_CH <- forest_CH %>% 
   st_make_grid(cellsize = 500, what = "polygons",
                square = FALSE) # This statements leads to hexagons
-track_sf <-   st_as_sf(track_sf, coords = c("long", "lat"), crs = 4326) %>%
-  st_transform(3035)
+
 tab <- st_intersects(grid_CH, track_sf)
 lengths(tab)
 countgrid <- st_sf(n = lengths(tab), geometry = st_cast(grid_CH, "MULTIPOLYGON")) %>%
@@ -276,7 +110,7 @@ dim(countgrid)
 
 
 summary(log(countgrid$n+1))
-pal <- colorNumeric(c("cornflowerblue","firebrick"), seq(0,10))
+pal <- colorNumeric(c("cornflowerblue","firebrick"), seq(0,12))
 leaflet(options = leafletOptions(zoomControl = F)) %>% #changes position of zoom symbol
   htmlwidgets::onRender("function(el, x) {L.control.zoom({ 
                            position: 'bottomright' }).addTo(this)}"
@@ -286,12 +120,26 @@ leaflet(options = leafletOptions(zoomControl = F)) %>% #changes position of zoom
   addProviderTiles("OpenTopoMap", group = "Roadmap", options = providerTileOptions(attribution = F)) %>%  
   addLayersControl(baseGroups = c("Satellite", "Roadmap")) %>%  
   
+  addCircleMarkers(
+    data=track_sf %>% st_transform(4326) %>% slice_sample(n=1000),
+    radius = 1,
+    stroke = TRUE, color = "goldenrod", weight = 0.8,
+    fillColor = "goldenrod", fillOpacity = 0.8
+  ) %>%
+  
   addPolygons(
-    data=countgrid %>%
-      st_transform(4326),
-    stroke = TRUE, color = ~pal(log(n+1)), weight = 1,
-    fillColor = ~pal(log(n+1)), fillOpacity = 0.5
+    data=st_as_sfc(st_bbox(track_sf %>% st_transform(4326))),
+    stroke = TRUE, color = "red", weight = 1,
+    fillOpacity = 0.5
   )
+  # 
+  # addPolygons(
+  #   data=grid_CH %>%
+  #     st_transform(4326),
+  #   stroke = TRUE, color = "green", weight = 1,
+  #   fillColor = "grey17", fillOpacity = 0.5
+  # )
+
 
 
 
@@ -320,7 +168,8 @@ DATA_CH <- DATA_CH %>%
 tabind <- st_intersects(countgrid, track_sf)
 lengths(tabind)
 for(c in 1:length(tabind)){
-  countgrid$N_ind[c]<-length(unique(track_sf$year_id[tabind[c][[1]]]))
+  countgrid$N_ind[c]<-length(unique(track_sf$bird_id[tabind[c][[1]]]))
+  #countgrid$N_ind[c]<-length(unique(track_sf$year_id[tabind[c][[1]]]))
 }
 
 #### COUNT N INDIVIDUALS AND PREDICTED FEEDING LOCS PER GRID CELL
@@ -331,7 +180,8 @@ OUT_sf<-DATA_CH %>%
 tabfeed <- st_intersects(countgrid, OUT_sf)
 countgrid$N_feed_points<-lengths(tabfeed)
 for(c in 1:length(tabfeed)){
-  countgrid$N_feed_ind[c]<-length(unique(OUT_sf$year_id[tabfeed[c][[1]]]))
+  #countgrid$N_feed_ind[c]<-length(unique(OUT_sf$year_id[tabfeed[c][[1]]]))
+  countgrid$N_feed_ind[c]<-length(unique(OUT_sf$bird_id[tabfeed[c][[1]]]))
 }
 
 
@@ -385,7 +235,7 @@ FEEDgrid<-CHgrid %>%
 
 
 
-saveRDS(CHgrid,"output/REKI_feeding_grid.rds")
+saveRDS(CHgrid,"output/REKI_feeding_grid2024.rds")
 
 
 
